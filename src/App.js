923e66e997a7cbbcf03a3b38bcceb04f54982940
Carlos Pajares <c.pajares@hotmail.com>
2021-11-06 16:22:50 +0100

    Created component robot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Robot from "./components/Robot/Robot";
 
 function App() {
   return (
     <div className="App">
-      <h1>HI!</h1>
+      <Robot />
     </div>
   );
 }
