9b20df4b1c2a624d264bf135c870b4b9bcde0330
Carlos Pajares <c.pajares@hotmail.com>
2021-11-09 15:03:15 +0100

    changed loginUserThunks

--- src/redux/thunks/userThunks.js
+++ src/redux/thunks/userThunks.js
@@ -1,21 +1,20 @@
+import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { loginUserAction } from "../actions/actionCreators";
 
 const apiUrl = process.env.REACT_APP_URL_API_HEROKU;
 
 export const loginUserThunks = (user) => async (dispatch) => {
-  const response = await fetch(`${apiUrl}login`, {
-    method: "POST",
-    body: JSON.stringify(user),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await axios.post(`${apiUrl}login`, user);
+  console.log("hola");
 
-  const loginUser = await response.json();
-  if (response.ok) {
-    const token = jwtDecode(response.token);
-    dispatch(loginUserAction(loginUser));
-    localStorage.setItem("tokenStorage", JSON.stringify({ token }));
+  if (response.status === 200) {
+    const token = response.data.token;
+    console.log(token);
+    const user = jwtDecode(token);
+    console.log(user);
+    /* dispatch(loginUserAction(loginUser)); */
+    /*     localStorage.setItem("tokenStorage", JSON.stringify({ token }));
+     */
   }
 };
